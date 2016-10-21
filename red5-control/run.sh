26748551392a77b7bdb84d7b8ea51266e52bed82
bogsolomon <bog.solomon@gmail.com>
2016-10-21 19:56:54 -0400

    red5-manager and red5-control docker file fixex

--- red5-control/run.sh
+++ red5-control/run.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-java -Djava.net.preferIPv4Stack=true -jar self_org_control.jar  /config/config.xml
+java -agentlib:jdwp=transport=dt_socket,server=y,address=8000,suspend=n -Djava.net.preferIPv4Stack=true -jar self_org_control.jar /config/config.xml
+
--- red5-manager/run.sh
+++ red5-manager/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Djava.net.preferIPv4Stack=true -jar self-org-manager.jar  /config/config.xml
+java -agentlib:jdwp=transport=dt_socket,server=y,address=8001,suspend=n -Djava.net.preferIPv4Stack=true -jar self-org-manager.jar  /config/config.xml
