33ea0bcd741f7bcaa93eea7d37094119291ae75f
bogsolomon <bog.solomon@gmail.com>
2016-10-28 21:13:35 -0400

    Using swarm in scripts + making run parameterized

--- scripts/clean-docker-run.sh
+++ scripts/clean-docker-run.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
-docker stop red5-manager-0
-docker stop red5-control-0
-docker stop red5-control-1
-docker stop red5-control-2
-docker stop red5-control-3
-docker stop red5-0
-docker stop red5-1
-docker stop red5-2
-docker stop red5-3
-docker rm red5-manager-0
-docker rm red5-control-0
-docker rm red5-control-1
-docker rm red5-control-2
-docker rm red5-control-3
-docker rm red5-0
-docker rm red5-1
-docker rm red5-2
-docker rm red5-3
+docker -H 172.30.4.2:4000 stop red5-manager-0
+docker -H 172.30.4.2:4000 stop red5-control-0
+docker -H 172.30.4.2:4000 stop red5-control-1
+docker -H 172.30.4.2:4000 stop red5-control-2
+docker -H 172.30.4.2:4000 stop red5-control-3
+docker -H 172.30.4.2:4000 stop red5-0
+docker -H 172.30.4.2:4000 stop red5-1
+docker -H 172.30.4.2:4000 stop red5-2
+docker -H 172.30.4.2:4000 stop red5-3
+docker -H 172.30.4.2:4000 rm red5-manager-0
+docker -H 172.30.4.2:4000 rm red5-control-0
+docker -H 172.30.4.2:4000 rm red5-control-1
+docker -H 172.30.4.2:4000 rm red5-control-2
+docker -H 172.30.4.2:4000 rm red5-control-3
+docker -H 172.30.4.2:4000 rm red5-0
+docker -H 172.30.4.2:4000 rm red5-1
+docker -H 172.30.4.2:4000 rm red5-2
+docker -H 172.30.4.2:4000 rm red5-3
--- scripts/run-docker-test.sh
+++ scripts/run-docker-test.sh
@@ -1,20 +1,43 @@
-#!/bin/sh
-docker run -d --net=red5 --name=red5-0 -e red5_port=1936 -e red5_ip=172.30.4.2 -p 1936:1935 bsolomon/red5-media:v1
-docker run -d --net=red5 --name=red5-1 -e red5_port=1937 -e red5_ip=172.30.4.2 -p 1937:1935 bsolomon/red5-media:v1
-docker run -d --net=red5 --name=red5-2 -e red5_port=1938 -e red5_ip=172.30.4.2 -p 1938:1935 bsolomon/red5-media:v1
-docker run -d --net=red5 --name=red5-3 -e red5_port=1939 -e red5_ip=172.30.4.2 -p 1939:1935 bsolomon/red5-media:v1
+#!/bin/bash
+numServers=$1
+time=$2
+start_port=1936
+echo "Running test for" $numServers "servers for" $time "minutes"
+totalNumServers=$(( $numServers - 1 ))
+for((i=0;i<=totalNumServers;i++))
+do
+	port=$(($start_port + $i))
+	docker -H 172.30.4.2:4000 run -d --net=red5 --name=red5-$i -e red5_port=$port -v /etc/hostname:/hostname -p $port:1935 bsolomon/red5-media:v1
+done
 sleep 30s
-docker run -d --net=red5 --name=red5-manager-0 -e is_seed=true -e red5_ip=172.30.4.2 -v /var/run/docker.sock:/var/run/docker.sock -v /usr/local/docker-images/red5-manager/config:/config bsolomon/red5-manager:v1
-nohup docker logs -f red5-manager-0 > /usr/local/logs/manager.log &
+docker -H 172.30.4.2:4000 run -d --net=red5 --name=red5-manager-0 -e is_seed=true -v /var/run/docker.sock:/var/run/docker.sock -v /usr/local/docker-images/red5-manager/config:/config bsolomon/red5-manager:v1
+nohup docker -H 172.30.4.2:4000 logs -f red5-manager-0 > /usr/local/logs/manager.log &
 sleep 10s
-docker run -d --net=red5 --name=red5-control-0 -e red5_port=1936 -e managed_host=red5-0 -e red5_ip=172.30.4.2 -v /usr/local/docker-images/red5-control/config:/config bsolomon/red5-control:v1
-nohup docker logs -f red5-control-0 > /usr/local/logs/control0.log &
-docker run -d --net=red5 --name=red5-control-1 -e red5_port=1937 -e managed_host=red5-1 -e red5_ip=172.30.4.2 -v /usr/local/docker-images/red5-control/config:/config bsolomon/red5-control:v1
-nohup docker logs -f red5-control-1 > /usr/local/logs/control1.log &
-docker run -d --net=red5 --name=red5-control-2 -e red5_port=1938 -e managed_host=red5-2 -e red5_ip=172.30.4.2 -v /usr/local/docker-images/red5-control/config:/config bsolomon/red5-control:v1
-nohup docker logs -f red5-control-2 > /usr/local/logs/control2.log &
-docker run -d --net=red5 --name=red5-control-3 -e red5_port=1939 -e managed_host=red5-3 -e red5_ip=172.30.4.2 -v /usr/local/docker-images/red5-control/config:/config bsolomon/red5-control:v1
-nohup docker logs -f red5-control-3 > /usr/local/logs/control3.log &
-sleep 15m
+for((j=0;j<=totalNumServers;j++))
+do
+	port=$(($start_port + $j))
+	docker -H 172.30.4.2:4000 run -d --net=red5 --name=red5-control-$j -e red5_port=$port -e managed_host=red5-$j -v /var/run/docker.sock:/var/run/docker.sock -v /usr/local/docker-images/red5-control/config:/config bsolomon/red5-control:v1
+	nohup docker -H 172.30.4.2:4000 logs -f red5-control-$j > /usr/local/logs/control$j.log &
+done
+sleep ${time}m
 ./clean-docker-run.sh
+
+#docker -H 172.30.4.2:4000 run -d --net=red5 --name=red5-0 -e red5_port=1936 -e red5_ip=172.30.4.2 -p 1936:1935 bsolomon/red5-media:v1
+#docker -H 172.30.4.2:4000 run -d --net=red5 --name=red5-1 -e red5_port=1937 -e red5_ip=172.30.4.2 -p 1937:1935 bsolomon/red5-media:v1
+#docker -H 172.30.4.2:4000 run -d --net=red5 --name=red5-2 -e red5_port=1938 -e red5_ip=172.30.4.2 -p 1938:1935 bsolomon/red5-media:v1
+#docker -H 172.30.4.2:4000 run -d --net=red5 --name=red5-3 -e red5_port=1939 -e red5_ip=172.30.4.2 -p 1939:1935 bsolomon/red5-media:v1
+#sleep 30s
+#docker -H 172.30.4.2:4000 run -d --net=red5 --name=red5-manager-0 -e is_seed=true -e red5_ip=172.30.4.2 -v /var/run/docker.sock:/var/run/docker.sock -v /usr/local/docker-images/red5-manager/config:/config bsolomon/red5-manager:v1
+#nohup docker -H 172.30.4.2:4000 logs -f red5-manager-0 > /usr/local/logs/manager.log &
+#sleep 10s
+#docker -H 172.30.4.2:4000 run -d --net=red5 --name=red5-control-0 -e red5_port=1936 -e managed_host=red5-0 -e red5_ip=172.30.4.2 -v /usr/local/docker-images/red5-control/config:/config bsolomon/red5-control:v1
+#nohup docker -H 172.30.4.2:4000 logs -f red5-control-0 > /usr/local/logs/control0.log &
+#docker -H 172.30.4.2:4000 run -d --net=red5 --name=red5-control-1 -e red5_port=1937 -e managed_host=red5-1 -e red5_ip=172.30.4.2 -v /usr/local/docker-images/red5-control/config:/config bsolomon/red5-control:v1
+#nohup docker -H 172.30.4.2:4000 logs -f red5-control-1 > /usr/local/logs/control1.log &
+#docker -H 172.30.4.2:4000 run -d --net=red5 --name=red5-control-2 -e red5_port=1938 -e managed_host=red5-2 -e red5_ip=172.30.4.2 -v /usr/local/docker-images/red5-control/config:/config bsolomon/red5-control:v1
+#nohup docker -H 172.30.4.2:4000 logs -f red5-control-2 > /usr/local/logs/control2.log &
+#docker run -H 172.30.4.2:4000 -d --net=red5 --name=red5-control-3 -e red5_port=1939 -e managed_host=red5-3 -e red5_ip=172.30.4.2 -v /usr/local/docker-images/red5-control/config:/config bsolomon/red5-control:v1
+#nohup docker -H 172.30.4.2:4000 logs -f red5-control-3 > /usr/local/logs/control3.log &
+#sleep 15m
+#./clean-docker-run.sh
 
